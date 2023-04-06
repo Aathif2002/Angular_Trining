9d4d09d983ad7ccfdfbfd5fe4261bed905683bb4
Aathif2002 <aathifpbt@gmail.com>
2023-04-06 13:41:12 +0530

    Trining

---
+++ SecoundAPP/practice.js
@@ -0,0 +1,33 @@
+
+/*
+function aathif()
+{
+    a=10;
+    b=20;
+    c=a+b
+    console.log(c)
+}
+
+mohammed=aathif
+mohammed()
+*/
+/*
+a=[1,2,3]
+a.push(4)
+console.log(a)
+b=a
+b.push(5)
+console.log(a)
+c=[]
+for(let x of b)
+{
+    c.push(x)
+}
+
+console.log(c)
+c.push("aathif")
+console.log(c)
+console.log(a) */
+
+
+
